1263261350602d8494265c04612e332226161881
KiranKumarMalik <malikkiran413@gmail.com>
2025-01-31 12:36:51 +0530

    new project

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def kiran(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def sagar(request):
+    return HttpResponse('Sagar is a Play Boy')
+
+def hello(request):
+    return HttpResponse("<h1>Hello Kiran</h1> \n <h2>Hello Sagar</h2> \n <h3>Hello Chiku</h3>")
+
+def youtube(request):
+    return HttpResponse("<h1> Watch Youtube Videos </h1>")
